e40ddb445ffe98c8a25c394d03ec962bc09e5240
Aniket Nikam <125387241+Anky597@users.noreply.github.com>
2025-01-14 21:47:49 +0530

    Update ProjectCard.tsx
    update

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -84,7 +84,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
             </div>
             <Button onClick={() => setIsModalOpen(true)}>
               <Info size={18} className="mr-2" />
-              More Info
+              More Info{Switch to Dark Mode}
             </Button>
           </CardFooter>
         </Card>
